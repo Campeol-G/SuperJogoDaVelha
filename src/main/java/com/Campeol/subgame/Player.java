450a6372195303409bb8722c71787ec6b4a49480
ChicoMagnusOpus <gustavocampeol28@gmail.com>
2026-08-07 13:59:51 -0300

    add Player

---
+++ src/main/java/com/Campeol/subgame/Player.java
@@ -0,0 +1,19 @@
+package com.Campeol.subgame;
+
+public class Player {
+
+  private Piece piece;
+
+  public Player(Piece piece) {
+    this.piece = piece;
+  }
+
+  public Piece getPiece() {
+    return piece;
+  }
+
+  public void setPiece(Piece piece) {
+    this.piece = piece;
+  }
+
+}
